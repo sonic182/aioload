6c32824af6edf7191446f80355fa68f9a259da85
Johanderson <johander1822@gmail.com>
2019-05-28 14:59:13 +0200

    minimal load test script

--- load_test/__init__.py
+++ load_test/__init__.py
@@ -2,14 +2,33 @@
 
 import asyncio
 import aiohttp
+import statistics
+from datetime import datetime
+from datetime import timedelta
 from argparse import ArgumentParser
 from configparser import ConfigParser
 
 from load_test.utils import get_logger
 
 
-async def request(session, url, config):
+async def request(session, sem, logger, config):
     """Do request and return statics."""
+    async with sem:
+        logger.debug('doing request')
+        before = datetime.now()
+        async with session.request(
+                method=config['test']['method'],
+                url=config['test']['url'],
+                params=dict(config['params']),
+                headers=dict(config['headers'])
+        ) as resp:
+            after = datetime.now()
+            res = {
+                'code': resp.status,
+                'duration': (after - before) / timedelta(milliseconds=1)
+            }
+            logger.debug('done request', extra=res)
+            return res
 
 
 async def start(logger, args, config):
@@ -21,7 +40,26 @@ async def start(logger, args, config):
             sock_connect=config['http'].getint('sock_connect', 3),
         )
     ) as session:
-        pass
+        sem = asyncio.Semaphore(args.concurrency)
+        statics = await asyncio.gather(*[
+            request(session, sem, logger, config)
+            for _ in range(args.number_of_requests)]
+        )
+        durations = [x['duration'] for x in statics]
+        codes = {}
+        for x in statics:
+            codes[x['code']] = code = codes.get(x['code'], 0)
+            codes[x['code']] = code + 1
+
+        logger.info('done', extra={
+            'min': min(durations),
+            'max': max(durations),
+            'mean': statistics.mean(durations),
+            'stdev': statistics.stdev(durations),
+            'codes': codes,
+            'concurrency': args.concurrency,
+            'requests': args.number_of_requests,
+        })
 
 
 def get_arguments():

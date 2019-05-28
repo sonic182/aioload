ebe69364b24db553ca45bb2b95f23015a038f1a4
sonic182 <johander1822@gmail.com>
2019-05-28 20:39:56 +0200

    Fix setup.py and travis

--- setup.py
+++ setup.py
@@ -1,16 +1,6 @@
 """Setup module."""
 
 from setuptools import setup
-try:
-    from pip._internal.req import parse_requirements
-except ImportError:
-    from pip.req import parse_requirements
-
-
-def requirements(filename):
-    """Parse requirements from requirements.txt."""
-    reqs = parse_requirements(filename, session=False)
-    return [str(req.req) for req in reqs]
 
 
 setup(
@@ -23,15 +13,16 @@ setup(
     packages=['load_test'],
     setup_requires=['pytest-runner'],
     test_requires=['pytest'],
-    install_requires=requirements('requirements.txt'),
+    install_requires=[
+        'aiohttp',
+        'cchardet',
+        'aiodns',
+    ],
     extras_require={
-        'dev': requirements('dev-requirements.txt'),
         'test': [
             'pytest',
             'pytest-cov',
-            'pytest-sugar',
-            'coverage',
-            'coveralls'
+            'pytest-asyncio',
         ]
     }
 )
